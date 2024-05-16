de228613d85c2e5c7dd21a1ae4ec5d7e9ddf84e6
KievMaia <kiev@prologapp.com>
2024-05-16 15:40:28 -0300

    Finaliza o controller.

--- src/main/java/br/com/zalf/prologappreports/controller/ReportController.java
+++ src/main/java/br/com/zalf/prologappreports/controller/ReportController.java
@@ -1,5 +1,9 @@
 package br.com.zalf.prologappreports.controller;
 
+import br.com.zalf.prologappreports.service.WorkOrderServiceReport;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/reports")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ReportController {
-    @GetMapping(path = "/", produces = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity<byte[]> helloWorld() {
-        return null;
+    private final WorkOrderServiceReport serviceReport;
+
+    @GetMapping(produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<byte[]> getReportPdf() {
+        final var bytesPdf = serviceReport.getReportDto();
+
+        final var headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=relatorio-de-servicos.pdf");
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_PDF)
+                .headers(headers)
+                .body(bytesPdf);
     }
 }
