687c296f19a64da0767a08079f9f08532d13d96a
KievMaia <kiev@prologapp.com>
2024-05-16 14:23:22 -0300

    Inica criação do endpoint, cria o controller.

---
+++ src/main/java/br/com/zalf/prologappreports/controller/ReportController.java
@@ -0,0 +1,17 @@
+package br.com.zalf.prologappreports.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/reports")
+public class ReportController {
+    public static final MediaType APPLICATION_PDF = null;
+    @GetMapping(path = "/", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<byte[]> helloWorld() {
+        return null;
+    }
+}
