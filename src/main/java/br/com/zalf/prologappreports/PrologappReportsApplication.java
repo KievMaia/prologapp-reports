dd902cdc7dcd9f0004b1a6eb9f4d511010112261
KievMaia <kiev@prologapp.com>
2024-05-16 11:52:05 -0300

    first commit

---
+++ src/main/java/br/com/zalf/prologappreports/PrologappReportsApplication.java
@@ -0,0 +1,13 @@
+package br.com.zalf.prologappreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrologappReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrologappReportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zalf/prologappreports/PrologappReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zalf.prologappreports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrologappReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
