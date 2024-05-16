492d6eb771d57d13422149d9a3319c7132c61f18
KievMaia <kiev@prologapp.com>
2024-05-16 15:40:45 -0300

    Cria o serviço de criação do relatório.

---
+++ src/main/java/br/com/zalf/prologappreports/service/WorkOrderServiceReport.java
@@ -0,0 +1,46 @@
+package br.com.zalf.prologappreports.service;
+
+import br.com.zalf.prologappreports.model.WorkOrderStratificationServiceReportDto;
+import lombok.RequiredArgsConstructor;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class WorkOrderServiceReport {
+    public byte[] getReportDto() {
+        try {
+            final var inputStream =
+                    new FileInputStream(
+                            "C:/dev/projects/PrologWebService/src/main/resources/relatorios/relatorio-de-servicos" +
+                                    "-realizados.jasper");
+
+            final var parameters = new HashMap<String, Object>();
+            parameters.put("REPORT_LOCALE", new Locale("pt", "BR"));
+
+            final var workOrderStratificationServiceReportDtos =
+                    Collections.nCopies(100, WorkOrderStratificationServiceReportDto.builder().build());
+            final var dataSource = new JRBeanCollectionDataSource(workOrderStratificationServiceReportDtos);
+
+            final var jasperPrint = JasperFillManager.fillReport(inputStream, parameters, dataSource);
+
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<WorkOrderStratificationServiceReportDto> crateWorkOrderStratificationServiceReportDto() {
+        return Collections.nCopies(100, WorkOrderStratificationServiceReportDto.builder().build());
+    }
+}
+
