44acc45f344dd908f7bfa3305d055ef07a41556b
KievMaia <kiev@prologapp.com>
2024-05-21 17:56:31 -0300

    Ajusta a ordem dos testes.

--- src/test/java/br/com/zalf/prologappreports/PrologAppReportsApplicationTests.java
+++ src/test/java/br/com/zalf/prologappreports/PrologAppReportsApplicationTests.java
@@ -1,16 +1,21 @@
 package br.com.zalf.prologappreports;
 
 import org.apache.jorphan.collections.ListedHashTree;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PrologAppReportsApplicationTests {
     final JmeterTestPlan testPlanClient = new JmeterTestPlan();
 
     @Test
+    @Order(1)
     public void shouldReturnSummaryOfResultsWhenTestTenThreadsAndFiveRequests() {
         final var httpRequests = new ArrayList<HttpRequestCreateObject>();
         httpRequests.add(HttpRequestCreateObject.builder()
@@ -33,11 +38,17 @@ class PrologAppReportsApplicationTests {
                                  .build());
         ListedHashTree createdTree =
                 testPlanClient.createTestPlan("Compare Pdf report and Checklist offline",
-                                              10,
                                               5,
+                                              1,
                                               httpRequests,
                                               "src/main/resources/results/results-compare.csv");
 
         testPlanClient.engineRunner(createdTree);
+    }
+
+    @Test
+    @Order(2)
+    public void generateHTMLDashboard() {
+        testPlanClient.htmlReportGenerator();
     }
 }
