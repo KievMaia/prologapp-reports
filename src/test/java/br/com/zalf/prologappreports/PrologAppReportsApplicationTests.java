86cf188a8b3e875d3f91e6a015fa198bde52f954
KievMaia <kiev@prologapp.com>
2024-05-21 12:04:06 -0300

    Ajusta a criação dos testes para realizar a comparação de desempenho dos endpoints.

--- src/test/java/br/com/zalf/prologappreports/PrologAppReportsApplicationTests.java
+++ src/test/java/br/com/zalf/prologappreports/PrologAppReportsApplicationTests.java
@@ -4,35 +4,39 @@ import org.apache.jorphan.collections.ListedHashTree;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+
 @SpringBootTest
 class PrologAppReportsApplicationTests {
+    final JmeterTestPlan testPlanClient = new JmeterTestPlan();
 
     @Test
     public void shouldReturnSummaryOfResultsWhenTestTenThreadsAndFiveRequests() {
-        final var testPlanClient = new JmeterTestPlan();
-        ListedHashTree createdTree = testPlanClient.createTestPLan("Jasper Reports PDF",
-                                                                   "localhost",
-                                                                   "/reports",
-                                                                   "GET",
-                                                                   10,
-                                                                   5,
-                                                                   "src/main/resources/results/results-10users" +
-                                                                           "-5requests.csv");
-
-        testPlanClient.engineRunner(createdTree);
-    }
-
-    @Test
-    public void shouldReturnSummaryOfResultsWhenTestOneThousandNinetySixThreadsAndOneRequestEach() {
-        final var testPlanClient = new JmeterTestPlan();
-        ListedHashTree createdTree = testPlanClient.createTestPLan("Jasper Reports PDF",
-                                                                   "localhost",
-                                                                   "/reports",
-                                                                   "GET",
-                                                                   1096,
-                                                                   1,
-                                                                   "src/main/resources/results/results-1096users" +
-                                                                           "-5requests.csv");
+        final var httpRequests = new ArrayList<HttpRequestCreateObject>();
+        httpRequests.add(HttpRequestCreateObject.builder()
+                                 .withDomainName("localhost")
+                                 .withPath("/reports")
+                                 .withHttpMethod("GET")
+                                 .withPort(8081)
+                                 .withProtocol("http")
+                                 .build());
+        httpRequests.add(HttpRequestCreateObject.builder()
+                                 .withDomainName("localhost")
+                                 .withPath(
+                                         "/prolog/v2/checklist-offline/offline-support?codUnidade=215" +
+                                                 "&forcarAtualizacao=true")
+                                 .withHttpMethod("GET")
+                                 .withPort(8080)
+                                 .withProtocol("http")
+                                 .withHeaderName("Authorization")
+                                 .withHeaderValue("Bearer 2jadvp5jd3gsvios90bmbi45g6")
+                                 .build());
+        ListedHashTree createdTree =
+                testPlanClient.createTestPlan("Compare Pdf report and Checklist offline",
+                                              10,
+                                              5,
+                                              httpRequests,
+                                              "src/main/resources/results/results-compare.csv");
 
         testPlanClient.engineRunner(createdTree);
     }
