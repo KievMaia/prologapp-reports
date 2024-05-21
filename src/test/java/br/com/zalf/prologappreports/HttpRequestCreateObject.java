9887a47f12ae792e5315e901dce3e2d8ec57121b
KievMaia <kiev@prologapp.com>
2024-05-21 12:03:21 -0300

    Cria objeto para criar a requisição HTTP.

---
+++ src/test/java/br/com/zalf/prologappreports/HttpRequestCreateObject.java
@@ -0,0 +1,24 @@
+package br.com.zalf.prologappreports;
+
+import lombok.Builder;
+import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Data
+@Builder(toBuilder = true, setterPrefix = "with")
+public class HttpRequestCreateObject {
+    @NotNull
+    private final String domainName;
+    @NotNull
+    private final String path;
+    @NotNull
+    private final String httpMethod;
+    private final int port;
+    @NotNull
+    private final String protocol;
+    @Nullable
+    private final String headerName;
+    @Nullable
+    private final String headerValue;
+}
